e329fb5a4d34eff6ba15a237500486700d501d33
Marco Acea <marcoacea@Marcos-MBP.wv.cc.cmu.edu>
2019-10-07 04:52:48 -0400

    Everything is working as expected

--- app/src/main/java/com/example/weekfiveandroiddev/Main2Activity.java
+++ app/src/main/java/com/example/weekfiveandroiddev/Main2Activity.java
@@ -9,6 +9,8 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 
@@ -22,26 +24,92 @@ public class Main2Activity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         final int mode = getIntent().getIntExtra("mode", 0);
+        EditText note_title = findViewById(R.id.note_title);
+        final int count_tmp = getIntent().getIntExtra("count", 0);
+
+        //Mode 2 makes a new note, Mode 3 opens an old one
+        if (mode == 2){
+            note_title.setText("Note" + count_tmp);
+        } else if (mode == 3){
+            String title = getIntent().getStringExtra("title");
+            String note = getIntent().getStringExtra("note");
+            note_title.setText(title);
+            EditText note_body = findViewById(R.id.note_body);
+            note_body.setText(note);
+        }
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                submitText(mode);
+                submitText(mode, getIntent());
             }
         });
     }
 
-    private void submitText(final int mode) {
+    private void submitText(final int mode, Intent i) {
         if (mode == 2) {
-            EditText editText = findViewById(R.id.edit_text);
+            //Get the two editText objects
+            EditText note_title = findViewById(R.id.note_title);
+            EditText note_body = findViewById((R.id.note_body));
             // Prepare data intent
             Intent data = new Intent();
-            // Pass relevant data back as a result
-            data.putExtra("text", editText.getText().toString());
+
+            // Gather the text from the two editText
+            String note_title_string = note_title.getText().toString();
+            String note_body_string = note_body.getText().toString();
+
+            //Put the strings into the data-intent object
+            data.putExtra("title", note_title_string);
+            data.putExtra("body", note_body_string);
+            data.putExtra("isNew", true);
+
             // Activity finished ok, return the data
             setResult(RESULT_OK, data); // set result code and bundle data for response
             finish(); // closes the activity, pass data to parent
         }
+        else if (mode == 3){
+            //Get the two editText objects
+            EditText note_title = findViewById(R.id.note_title);
+            EditText note_body = findViewById((R.id.note_body));
+            // Prepare data intent
+            Intent data = new Intent();
+
+            // Gather the text from the two editText
+            String note_title_string = note_title.getText().toString();
+            String note_body_string = note_body.getText().toString();
+
+            //Put the strings into the data-intent object
+            data.putExtra("title", note_title_string);
+            data.putExtra("body", note_body_string);
+            data.putExtra("isNew", false);
+            data.putExtra("index", i.getExtras().getInt("index"));
+
+            // Activity finished ok, return the data
+            setResult(RESULT_OK, data); // set result code and bundle data for response
+            finish(); // closes the activity, pass data to parent
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/example/weekfiveandroiddev/MainActivity.java
+++ app/src/main/java/com/example/weekfiveandroiddev/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.weekfiveandroiddev;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 
@@ -9,28 +10,69 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.view.LayoutInflater;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     private final int REQUEST_CODE = 20;
+    private int count = 0;
+    private ArrayList<String> titles = new ArrayList<String>();
+    private ArrayList<String> notes = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //Sets the content to view to activity_main.xml
         setContentView(R.layout.activity_main);
+
+        //Toolbar is the green tab at the top of the screen
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        //Create the listView object
+        ListView note_list = (ListView)findViewById(R.id.note_list);
+        CustomAdapter customAdapter = new CustomAdapter(getApplicationContext(), titles, notes);
+        note_list.setAdapter(customAdapter);
+
+        note_list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> list, View v, int pos, long id) {
+                Intent i = new Intent(MainActivity.this, Main2Activity.class);
+                i.putExtra("mode", 3); // pass arbitrary data to launched activity
+                i.putExtra("title", titles.get(pos));
+                i.putExtra("note", notes.get(pos));
+                i.putExtra("index", pos);
+                startActivityForResult(i, REQUEST_CODE);
+            }
+        });
+
+        //create_note button is the button at the bottom right of the screen
+        FloatingActionButton create_note_button = findViewById(R.id.create_note);
+
+        //Adds an Click Listener to the create_note_button
+        create_note_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent i = new Intent(MainActivity.this, Main2Activity.class);
                 i.putExtra("mode", 2); // pass arbitrary data to launched activity
+                i.putExtra("count", count); //pass in the number of notes
                 startActivityForResult(i, REQUEST_CODE);
             }
         });
@@ -41,32 +83,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         // REQUEST_CODE is defined above
         if (resultCode == RESULT_OK && requestCode == REQUEST_CODE) {
-            // Extract name value from result extras
-            String name = data.getExtras().getString("text");
-            // Toast the name to display temporarily on screen
-            Toast.makeText(this, name, Toast.LENGTH_SHORT).show();
+            if (data.getBooleanExtra("isNew", false)) {
+                // Extract name value from result extras
+                String title = data.getExtras().getString("title");
+                String note = data.getExtras().getString("body");
+                titles.add(title);
+                notes.add(note);
+
+                //Create the listView object
+                ListView note_list = (ListView) findViewById(R.id.note_list);
+                CustomAdapter customAdapter = new CustomAdapter(getApplicationContext(), titles, notes);
+                note_list.setAdapter(customAdapter);
+
+                count++;
+
+                // Let the user know their note has been created
+                Toast.makeText(this, title + " created.", Toast.LENGTH_SHORT).show();
+            } else {
+                // Extract name value from result extras
+                String title = data.getExtras().getString("title");
+                String note = data.getExtras().getString("body");
+                int i = data.getExtras().getInt("index");
+
+                //Edit the values in titles and notes at that index
+                titles.set(i, title);
+                notes.set(i, note);
+
+                //Create the listView object
+                ListView note_list = (ListView) findViewById(R.id.note_list);
+                CustomAdapter customAdapter = new CustomAdapter(getApplicationContext(), titles, notes);
+                note_list.setAdapter(customAdapter);
+
+                // Let the user know their note has been edited
+                Toast.makeText(this, title + " has been updated.", Toast.LENGTH_SHORT).show();
+            }
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
